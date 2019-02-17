aa6093a5705d863ba45fbb4ad84b6f7a0fc6a344
David Cohen <cohen.983@osu.edu>
2019-02-16 22:37:40 -0500

    Rewrote movement function for significant gains in self-correction in the event of poorly paired motors.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # feh-robot
 Serves as a repository for the code written to run team Star Trak's robot.
 Code in this repository is intended to be run on the Ohio State University's Proteus controller, maintained by the department of Engineering Education.
+## Functions
+### Move
+Takes an argument of the form (int x, float y).
+The x argument defines power percentage, and the y argument defines distance to move.
+The y argument provided can be of either sign, the x argument should be positive.
--- main.cpp
+++ main.cpp
@@ -11,37 +11,12 @@ FEHMotor rightMotor(FEHMotor::Motor0,9.0);
 FEHMotor leftMotor(FEHMotor::Motor1,9.0);
 
 DigitalEncoder rightEncoder(FEHIO::P0_0);
-DigitalEncoder LeftEncoder(FEHIO::P0_1);
+DigitalEncoder leftEncoder(FEHIO::P0_1);
 
 FEHServo tinyServo(FEHServo::Servo0);
 
-
-/***********************
- * Code to move Forward*
- * *********************/
-void MoveForward(int powerPercent, float inches){
-    rightMotor.SetPercent(powerPercent);
-    leftMotor.SetPercent(-powerPercent);
-
-    //Calculate rotations needed to travel that distance
-    rotations = inches/CIRCUMFRENCE;
-    encoderTicks=rotations * 318;
-
-    //Wait for Robot to traverse distance
-
-    bool leftDone = false, rightDone = false;
-    while (!leftDone || !rightDone){
-        if (LeftEncoder.Counts()>= encoderTicks){
-            leftDone = true;
-            leftMotor.SetPercent(0);
-        }
-        if (rightEncoder.Counts()>=encoderTicks){
-            rightDone = true;
-            rightMotor.SetPercent(0);
-        }
-    }
-}
-
+void Move(int x, float y);
+//Given an integer argument for power and a float argument for distance, moves the robot that far at that speed
 
 int main(void)
 {
@@ -57,9 +32,8 @@ int main(void)
 
 
     LCD.WriteLine("Awesome");
-    rightMotor.SetPercent(0);
-    leftMotor.SetPercent(0);
-
+    rightMotor.Stop();
+    leftMotor.Stop();
 
     tinyServo.SetMin(540);
     tinyServo.SetMax(2469);
@@ -67,3 +41,98 @@ int main(void)
 
 }
 
+/*
+    Code to move robot in a straight line
+*/
+void Move(int x, float y)
+{
+    int powerPercent = x;
+    float distanceToMove = y;
+    bool negativeDistance = false;
+    float rotations = 0;
+    int encoderTicks = 0;
+
+    if(distanceToMove < 0) //throw a flag for a negative distance
+        negativeDistance = true;
+
+    //Calculate rotations needed to travel that distance
+    if(negativeDistance == false)
+    {
+        rotations = distanceToMove/CIRCUMFRENCE;
+        encoderTicks = rotations * 318;
+    }
+    else if(negativeDistance == true)
+    {
+        rotations = -1*distanceToMove/CIRCUMFRENCE;
+        encoderTicks = rotations * 318;
+    }
+
+    //Move the robot that number of rotations, balancing speed as it goes
+    bool moveComplete = false;
+    
+    int leftCoeff = -1; //add in the negative bit for the left motor
+    int rightCoeff = 1;
+    int rightCounts = 0;
+    int leftCounts = 0;
+    float rightCountsRatio = 0;
+    float leftCountsRatio = 0;
+
+    if(negativeDistance == true) //flip direction for a negative move
+    {
+        leftCoeff = 1;
+        rightCoeff = 1;
+    }
+
+    rightMotor.SetPercent(rightCoeff*powerPercent);
+    leftMotor.SetPercent(leftCoeff*powerPercent);
+
+    while(moveComplete == false)
+    {
+        leftCounts = leftEncoder.Counts();
+        rightCounts = rightEncoder.Counts();
+        rightCountsRatio = leftCounts / rightCounts; //used to determine magnitude of speed cut if one motor is moving faster, roughly
+        leftCountsRatio = rightCounts / leftCounts;
+
+        if(rightCounts > leftCounts)
+        {
+            rightMotor.SetPercent((rightCoeff*powerPercent)*rightCountsRatio);
+            leftMotor.SetPercent(leftCoeff*powerPercent);
+        }
+
+        else if(leftCounts < rightCounts)
+        {
+            rightMotor.SetPercent(rightCoeff*powerPercent);
+            leftMotor.SetPercent((leftCoeff*powerPercent)*leftCountsRatio);
+        }
+
+        else if(leftCounts == rightCounts)
+        {
+            rightMotor.SetPercent(rightCoeff*powerPercent);
+            leftMotor.SetPercent(leftCoeff*powerPercent);
+        }
+
+        else if(rightCounts >= encoderTicks && leftCounts >= encoderTicks)
+        // if either motor is past the threshold, stop the move and balance it out at the finish
+        {
+            rightMotor.Stop(); //stop them
+            leftMotor.Stop();
+            while(rightCounts != leftCounts) // then verify that they went the same distance
+            {
+                leftCounts = leftEncoder.Counts();
+                rightCounts = rightEncoder.Counts();
+
+                if(rightCounts > leftCounts) // if not, balance it out
+                {
+                    leftMotor.SetPercent(leftCoeff*powerPercent);
+                }
+                else if(leftCounts > rightCounts)
+                {
+                    rightMotor.SetPercent(rightCoeff*powerPercent);
+                }
+            }
+            rightEncoder.ResetCounts();
+            leftEncoder.ResetCounts();
+            moveComplete = true;
+        }
+    }
+}
